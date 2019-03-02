a82220de163f7bbc05fc3d3a5e9c6bed64cbf514
Rohit Joshi <joshi.rohit100@gmail.com>
2019-03-02 14:48:32 +0530

    Adding sample migration from mongodb with sample source plugin.

---
+++ modules/examples/apm_mongo_migrate/README.md
@@ -0,0 +1,37 @@
+# MongoDB Migrate
+
+This is a sample module to migrate data from MongoDB to Drupal8.
+
+## Requirements
+
+* This module requires php `mongodb` extension to be enabled.
+* Follow/See `http://php.net/manual/en/set.mongodb.php` link to
+install the `mongodb` php extension.
+* Once extension is installed, verify that extension is available
+by checking `php -i | grep 'mongo'` or just visiting phpinfo() page.
+* Once extension is installed, need to install the 'mongodb' PHP
+library as well.
+* Follow/See `http://php.net/manual/en/mongodb.tutorial.library.php`
+to install mongodb php library.
+
+## Installation
+* Once `mongodb` extension and PHP library is installed, update the
+settings.php with the `mongodb` database details like below -
+```
+$settings['mongodb'] = [
+  // Connection info.
+  'default' => [
+    'uri' => 'mongodb://127.0.0.1:27017',
+    'uriOptions' => [],
+    'driverOptions' => [],
+  ],
+  // Database name.
+  'database' => 'mongodb database name here',
+];
+ ```
+* Once DB settings done, import the data from the `data` directory
+of this module to your mongodb database by command -
+`./mongorestore -d <database_name> <data_directory_path>`
+* Command to export mongodata -
+`./mongodump -d <database_name> -o <directory_backup>`
+* Then enable the module `apm_mongo_migrate` and run migration.
---
+++ modules/examples/apm_mongo_migrate/src/Plugin/migrate/source/ApmMongo.php
@@ -0,0 +1,149 @@
+<?php
+namespace Drupal\apm_mongo_migrate\Plugin\migrate\source;
+
+use Drupal\Core\Site\Settings;
+use Drupal\migrate\Plugin\migrate\source\SourcePluginBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\migrate\Plugin\MigrationInterface;
+use MongoDB\Client;
+use MongoDB\Model\BSONDocument;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Source plugin for the MongoDB.
+ *
+ * @MigrateSource(
+ *   id = "apm_mongo"
+ * )
+ */
+class ApmMongo extends SourcePluginBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * MongoDB database connection info.
+   *
+   * @var array
+   */
+  protected $mongoDBConnection;
+
+  /**
+   * Source DB alias.
+   *
+   * @var string
+   */
+  protected $sourceCollection;
+
+  /**
+   * Source fields.
+   *
+   * @var array
+   */
+  protected $sourceFields;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition, MigrationInterface $migration = NULL) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $migration
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, MigrationInterface $migration) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $migration);
+    // @see settings.php for connection info.
+    $this->mongoDBConnection = Settings::get('mongodb');
+    $this->sourceCollection = $configuration['collection'];
+    $this->sourceFields = $configuration['fields'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fields() {
+    return $this->sourceFields;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __toString() {
+    $summary = [];
+    $summary[] = 'Alias: ' . $this->mongoDBConnection['database'];
+    $summary[] = 'Collection: ' . $this->sourceCollection;
+    return implode(', ', $summary);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getIds() {
+    return [
+      '_id' => [
+        'type' => 'string',
+        'length' => 24,
+        'not null' => TRUE,
+        'description' => 'MongoDB ID field.',
+      ],
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function initializeIterator() {
+    $source_data = $this->getSourceData();
+    return new \ArrayIterator($source_data);
+  }
+
+  /**
+   * @return mixed
+   */
+  protected function getSourceData() {
+    $rows = $this->getDatabase()
+      ->selectCollection($this->mongoDBConnection['database'], $this->sourceCollection)
+      ->find([])
+      ->toArray();
+    return $this->getDataAsArray($rows);
+  }
+
+  /**
+   * Function to get data as array.
+   *
+   * @param mixed $data
+   *   Array / Object / Final value.
+   *
+   * @return array
+   *   Array of records.
+   */
+  protected function getDataAsArray($data) {
+    $records = [];
+
+    if (!empty($data)) {
+      foreach ($data as $key => $value) {
+        if ($value instanceof BSONDocument) {
+          $records[$key] = (array) $value->jsonSerialize();
+        }
+      }
+    }
+
+    return $records;
+  }
+
+  /**
+   * Create and get mongodb connection object.
+   *
+   * @return Client
+   *   Mongo client object.
+   */
+  protected function getDatabase() {
+    $connection_info = $this->mongoDBConnection['default'];
+    return new Client($connection_info['uri'], $connection_info['uriOptions'], $connection_info['driverOptions']);
+  }
+
+}
